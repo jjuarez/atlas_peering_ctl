5f9d452d98301274ac15b2884992f6f663e85973
Javier Juarez <javier.juarez@fon.com>
2018-03-07 23:31:28 +0100

    New path for the files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
-require 'atlas_peering'
+require 'atlas'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure

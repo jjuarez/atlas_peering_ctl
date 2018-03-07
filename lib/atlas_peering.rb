ffd182878f1b56ec93cb1d4d7120df6ddf1e82eb
Javier Juarez <javier.juarez@fon.com>
2018-03-07 23:26:39 +0100

    Old naming

--- lib/atlas_peering.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require 'atlas_peering/version'
-require 'atlas_peering/cli'
--- lib/atlas_peering/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module AtlasPeering
-  VERSION = '0.1.1'
-end

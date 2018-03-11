9046b44926766469bb2a3ac22512ffeeddf351e5
Javier Juarez <javier.juarez@fon.com>
2018-03-11 11:38:06 +0100

    Adds the logging support

---
+++ lib/atlas/peering/logger.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Atlas
+  module Peering
+    ##
+    # class: Atlas::Peering::Logger: The application logger
+    class Logger
+      LOG_LEVELS = {
+        debug: ::Logger::DEBUG,
+        info:  ::Logger::INFO,
+        warn:  ::Logger::WARN,
+        error: ::Logger::ERROR
+      }.freeze
+
+      def self.configure(level, target = STDOUT)
+        logger       = ::Logger.new(target)
+        logger.level = LOG_LEVELS.fetch(level, ::Logger::INFO)
+
+        logger
+      end
+    end
+  end
+end
